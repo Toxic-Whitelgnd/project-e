840c724caca6f6f91b22665271736da8bc7b8506
Toxic-Whitlgnd <whitelegend56@gmail.com>
2023-01-08 19:19:42 +0530

    added blogpages

--- src/lib/client.js
+++ src/lib/client.js
@@ -6,6 +6,5 @@ export const Client = sanityClient(
         dataset:'production',
         apiVersion:'2023-01-08',
         useCdn: true,
-        token: 'skkkTJYME4xtyHmK5XKX6kEvExy6m9NEiKl0pjpcyQdHQwt7pzE1Y4YY70Ej05yvPdElWKL2quPNBlWwrEYbu7E6V75ATGcAkrDic2DFiE66oxucSHlI17NmqSUqcQLe4vMWkue1u3ftVJhBlStVUhp6svKDPF9KCLCbOiJlj4oGlGOCa1p4'
     }
 ) 
