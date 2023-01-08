a6080e516beef808c824a845a94b748ce62720af
hari <harikarumuri1086@gmail.com>
2023-01-08 19:25:01 +0530

    added footer design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from './Components/Hero/Hero';
 import NavBar from './Components/Navbar/Navbar';
+import WebsiteBuilder from './Components/Website-Card-Section/WebsiteBuilder';
+import Footer from './Components/Footer/Footer';
+
 
 function App() {
   return (
