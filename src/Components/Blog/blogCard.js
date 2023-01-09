f69ecf6c64e2cb0aed03ef38a83d4e146e66a62f
hari <harikarumuri1086@gmail.com>
2023-01-09 16:58:08 +0530

    resolved

--- src/Components/Blog/blogCard.js
+++ src/Components/Blog/blogCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion"
+import { motion} from "framer-motion"
+import ButtonAni from './../button-animation/Button-ani';
+
 
 
 const blogCard = ({ title, img, goTo, author }) => {
