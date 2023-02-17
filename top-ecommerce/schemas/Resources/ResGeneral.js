4d9506c6dbcf57f6f196a02a124f136b046b48a7
Toxic-Whitlgnd <whitelegend56@gmail.com>
2023-02-17 15:28:50 +0530

    added resources backend

---
+++ top-ecommerce/schemas/Resources/ResGeneral.js
@@ -0,0 +1,12 @@
+export default {
+    name:'resourcesgenerals',
+    title: 'Typeof Resources',
+    type:'document',
+    fields:[
+        {
+            name:'resourcestype',
+            title:'Resource type',
+            type:'string',
+        }
+    ]
+}
