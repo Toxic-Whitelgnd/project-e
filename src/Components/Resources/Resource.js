346e0663110ae304bf0fcf9d3a3e1437085c7ebc
hari <harikarumuri1086@gmail.com>
2023-02-17 16:05:44 +0530

    resouces done

--- src/Components/Resources/Resource.js
+++ src/Components/Resources/Resource.js
@@ -1,13 +1,46 @@
-import React from 'react'
-
-
+import React, { useState, useEffect } from "react";
+import { Client } from "../../lib/client";
+import { Link } from "react-router-dom";
+import { format } from "date-fns";
+import ButtonAni from "./../button-animation/Button-ani";
 
 const Resource = () => {
+  const [Resourcepost, setResourcepost] = useState([]);
+  useEffect(() => {
+    Client.fetch(
+      `*[_type == "respost"] {
+            title,
+            body,
+            description,
+            id,
+            link,
+          } `
+    )
+      .then((data) => {
+        setResourcepost(data);
+        console.log(data);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
     <>
       <h1>Fantastic Resources & Tools for Ecommerce (Jan 2023)</h1>
+      {
+        Resourcepost[0] && (
+          <div>
+            {
+              Resourcepost.map((story)=>(
+                <>
+                  <h1>{story.description}</h1>
+                </>
+              ))
+            }
+          </div>
+        )
+      }
     </>
-  )
-}
+  );
+};
 
-export default Resource
+export default Resource;
