637ffe26c46be326706fa77f127ee54fbf0f9eb5
hari <harikarumuri1086@gmail.com>
2023-03-22 02:09:34 +0530

    comparision added

--- README.md
+++ README.md
@@ -68,3 +68,12 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+# The Notes
+
+1.NewsLetter - Used convertkit;
+2.Routing - React Router 
+3.Icons
+4.Backend - Sanity
---
+++ src/Components/Comparision/comparision.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ButtonAni from './../button-animation/Button-ani';
+
+
+const comparision = () => {
+  return (
+    <div>
+      {/* <div className="col-lg-4 position-relative">
+        <div class="card pt-5 ">
+          <div className="topBadge">{props.badge}</div>
+          <div className="row">
+            <div className="col-lg-4 ml-2">
+              <img
+                src={props.logo}
+                className="card-img-custom img-fluid"
+                alt="..."
+                height="65px"
+              />
+            </div>
+            <div className="col-lg-8">
+              <h5 class="card-title">{props.title}</h5>
+              <h6>Rating:{props.rating}</h6>
+              <a href={props.reviewLink}>Read Full Review</a>
+            </div>
+          </div>
+
+          <div class="card-body ">
+            <p class="card-text text-wrap">
+              {props.desc.slice(0,55)}...
+            </p>
+            <div className="d-flex justify-content-center align-items-center">
+              <a href={props.siteLink}>
+              <ButtonAni widht={'95px'} height={'45px'} text={'Visit Site'} 
+                      background={'#7e57c2'} color={'#000'} hoverbackground={'#8659c2'} hovercolor={'d9aef4'} borderRad={'15px'} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  )
+}
+
+export default comparision
